a73fdc1a2bdd12429411ed1000b741b3a8846014
clusty <clusty1@gmail.com>
2014-02-18 07:18:32 -0500

    fixed query

--- light sensor/server side/light/fetchData.php
+++ light sensor/server side/light/fetchData.php
@@ -3,7 +3,7 @@
 $dbconn = pg_connect( "host=slackbox dbname=sensor user=pi password='raspberry'" );
 // Performing SQL query
 $query  = //'SELECT * FROM light';
-    "SELECT DATE_PART('hour',DATE_TRUNC('hour', timestamp)) AS timestamp, AVG(value) AS value FROM light WHERE timestamp > NOW()-INTERVAL '24 hours' GROUP by DATE_TRUNC('hour',timestamp) ORDER BY timestamp";
+    "SELECT DATE_TRUNC('hour', timestamp) AS timestamp, AVG(value) AS value FROM light WHERE timestamp > NOW()-INTERVAL '24 hours' GROUP by DATE_TRUNC('hour',timestamp) ORDER BY timestamp";
 $result = pg_query( $query ) or die( 'Query failed: ' . pg_last_error() );
 $rows = array( );
 while ( $line = pg_fetch_array( $result, null, PGSQL_ASSOC ) ) {
