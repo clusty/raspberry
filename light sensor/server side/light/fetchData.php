1a93ad2c4bb293edefb8f222f9ad27e34942c515
clusty <clusty1@gmail.com>
2014-03-01 12:21:10 -0500

    google graphs works

--- light sensor/server side/light/fetchData.php
+++ light sensor/server side/light/fetchData.php
@@ -3,7 +3,7 @@
 $dbconn = pg_connect( "host=slackbox dbname=sensor user=pi password='raspberry'" );
 // Performing SQL query
 $query  = //'SELECT * FROM light';
-"SELECT DATE_TRUNC('hour', time) AS time, AVG(value) AS value FROM sensors WHERE (id = $1) GROUP by DATE_TRUNC('hour',time) ORDER BY time LIMIT 100";
+"SELECT DATE_TRUNC('hour', time) AS time, AVG(value) AS value FROM sensors WHERE ( time > NOW()-INTERVAL '1 weeks' AND id = $1) GROUP by DATE_TRUNC('hour',time) ORDER BY time";
 $result = pg_query_params( $dbconn, $query, explode(' ',$_POST['id']) ) or die( 'Query failed: ' . pg_last_error() );
 $rows = array( );
 while ( $line = pg_fetch_array( $result, null, PGSQL_ASSOC ) ) {
